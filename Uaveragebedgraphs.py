1b08bed6ef15f174a9ee579a640cd7ffa161f948
Stefan Kurtenbach <Stefan.Kurtenbach@me.com>
2017-04-28 17:59:45 -0400

    Update Uaveragebedgraphs.py
    fixed a small bug

--- Uaveragebedgraphs.py
+++ Uaveragebedgraphs.py
@@ -1,4 +1,4 @@
-# Uaveragebedgraphs v2.0
+# Uaveragebedgraphs v2.1
 
 import argparse
 import os
@@ -18,56 +18,51 @@ def add (input, newfile):
     while done == "no":
 
 # get current chromosome
-        with open(input[0]) as ctrl:
-            current_chromosome = ""
-            done = "yes"
-            for line in ctrl:
-                line_split = line.split("\t")
-                line_split[3] = line_split[3].replace("\n", "")
-                if line_split[0] not in chromosomes_done:
-                    chromosomes_done.append(line_split[0])
-                    current_chromosome = line_split[0]
-                    done = "no"
-                    break
-            if done == "yes":
-                break
+        breakyn = "no"
+        for file in input:
+            if breakyn == "yes": break
+            with open(file) as f:
+                current_chromosome = ""
+                for line in f:
+                    line_split = line.split("\t")
+                    line_split[3] = line_split[3].replace("\n", "")
+                    if line_split[0] not in chromosomes_done:
+                        chromosomes_done.append(line_split[0])
+                        current_chromosome = line_split[0]
+                        breakyn = "yes"
+                        break
 
 # get start and stop for current chromosome
         stop = 0
-
         for file in input: # go through all files
             with open(file) as f:
-                breakyn = "no"
-                found = "no" #was chromosome found?
-
                 for line in f:
-                    if breakyn == "yes": break # if passed the current chromosome (no need to check the rest of the file)
                     line_split = line.split("\t")
-
                     if line_split[0] == current_chromosome:
-                        if line_split[3] != 0:
-                            if line_split[2] > stop:
-                                stop = int(line_split[2])
-                    elif found == "yes": #if passed chromosome trigger break
-                        breakyn = "yes"
-
+                        if int(line_split[2]) > stop:
+                            stop = int(line_split[2])
         if stop > 0:  #If there were values in that chromosome
             output = array('f', [0] * stop)
 
 #sum up
             for file in input:
                 with open(file) as f:
-                    breakyn = "no"
                     for line in f:
                         line_split = line.split("\t")
                         if line_split[0] == current_chromosome:
                             if float(line_split[3]) > 0:
-                                breakyn = "yes"
                                 line_split[3] = line_split[3].replace("\n", "")
                                 for o in range(int(line_split[2]) - int(line_split[1])):
                                     coord = o + int(line_split[1])
-                                    output[coord] = output[coord] + float(line_split[3])/len(input)
-                        elif breakyn == "yes": break
+                                    try:
+                                        output[coord] = output[coord] + float(line_split[3])/len(input)
+                                    except:
+                                        print("array_len: " + str(len(output)))
+                                        print("stop:" + str(len(output)))
+                                        print("o:" + str(o))
+                                        print("coord:" + str(coord))
+                                        print("line_split:" + str(line_split))
+                                        print("line_split3:" + str(line_split[3]))
 
 #concatenate entries with same value
             output2 = []
